f1d9fdb991f0556e5f8d14fc59fd7dfc3b10289a
deepto <ddepp98@outlook.com>
2023-06-01 16:57:44 +0530

    Initialized project, added util functions in lib

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::time::{ SystemTime, UNIX_EPOCH };
+
+// Current time in seconds
+pub fn now () -> u128 {
+    let duration = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+    ;
+
+    duration.as_secs() as u128 * 1000 + duration.subsec_millis() as u128
+}
+
+// Convert 32 bit int into array of 4 8 bit ints
+pub fn u32_bytes (u: &u32) -> [u8; 4] {
+    [
+        (u >> 8 * 0x0) as u8,
+        (u >> 8 * 0x1) as u8,
+        (u >> 8 * 0x2) as u8,
+        (u >> 8 * 0x3) as u8,
+    ]
+}
+
+// Convert 64 bit int into array of 8 8 bit ints
+pub fn u64_bytes (u: &u64) -> [u8; 8] {
+    [
+        (u >> 8 * 0x0) as u8,
+        (u >> 8 * 0x1) as u8,
+        (u >> 8 * 0x2) as u8,
+        (u >> 8 * 0x3) as u8,
+
+        (u >> 8 * 0x4) as u8,
+        (u >> 8 * 0x5) as u8,
+        (u >> 8 * 0x6) as u8,
+        (u >> 8 * 0x7) as u8,
+    ]
+}
+
+// Convert 128 bit int into array of 16 8 bit ints
+pub fn u128_bytes (u: &u128) -> [u8; 16] {
+    [
+        (u >> 8 * 0x0) as u8,
+        (u >> 8 * 0x1) as u8,
+        (u >> 8 * 0x2) as u8,
+        (u >> 8 * 0x3) as u8,
+
+        (u >> 8 * 0x4) as u8,
+        (u >> 8 * 0x5) as u8,
+        (u >> 8 * 0x6) as u8,
+        (u >> 8 * 0x7) as u8,
+
+        (u >> 8 * 0x8) as u8,
+        (u >> 8 * 0x9) as u8,
+        (u >> 8 * 0xa) as u8,
+        (u >> 8 * 0xb) as u8,
+
+        (u >> 8 * 0xc) as u8,
+        (u >> 8 * 0xd) as u8,
+        (u >> 8 * 0xe) as u8,
+        (u >> 8 * 0xf) as u8,
+    ]
+}
+
