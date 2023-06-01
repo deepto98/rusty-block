08a0e8322b654566d6e6eaa7be2d6d5cf82b76f1
deepto <ddepp98@outlook.com>
2023-06-02 02:56:44 +0530

    Implemented hashable for Transaction and output

--- src/transaction.rs
+++ src/transaction.rs
@@ -4,7 +4,39 @@ pub struct Output {
     pub value: u64,
 }
 
+impl Hashable for Output {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.extend(self.to_addr.as_bytes());
+        bytes.extend(&u64_bytes(&self.value));
+
+        bytes
+    }
+}
+
 pub struct Transaction {
     pub inputs: Vec<Output>,
     pub outputs: Vec<Output>,
+}
+
+impl Hashable for Transaction {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+
+        // TODO : Learn flat_map in depth
+        bytes.extend(
+            self.inputs
+                .iter()
+                .flat_map(|input| input.bytes()) //Lambda function - flattens vectors
+                .collect::<Vec<u8>>(),
+        );
+
+        bytes.extend(
+            self.outputs
+                .iter()
+                .flat_map(|output| output.bytes()) //Lambda function - flattens vectors
+                .collect::<Vec<u8>>(),
+        );
+        bytes
+    }
 }
