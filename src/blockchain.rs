f0e0741e823b11d908f4a8f1210f490af44a761a
deepto <ddepp98@outlook.com>
2023-06-02 01:46:05 +0530

    Implemented index verification

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,3 +3,16 @@ use super::*;
 pub struct Blockchain {
     pub blocks: Vec<Block>,
 }
+
+impl Blockchain {
+    pub fn verify(&self) -> bool {
+        for (i, block) in self.blocks.iter().enumerate() {
+            // 1. Check if indices match
+            if block.index != i as u32 {
+                println!("Index mismatch {} != {}", &block.index, &i);
+                return false;
+            }
+        }
+        true
+    }
+}
