e8760f7edcf363659a135f9bf2f5d5731421f7bd
deepto <ddepp98@outlook.com>
2023-06-02 03:07:07 +0530

    Added helper functions for Transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::collections::HashSet;
 pub struct Output {
     pub to_addr: Address,
     pub value: u64,
@@ -17,6 +18,37 @@ impl Hashable for Output {
 pub struct Transaction {
     pub inputs: Vec<Output>,
     pub outputs: Vec<Output>,
+}
+
+// Helpers for Transaction
+impl Transaction {
+    // Sum of all inputs
+    pub fn input_value(&self) -> u64 {
+        self.inputs.iter().map(|input| input.value).sum()
+    }
+
+    // Sum of all inputs
+    pub fn output_value(&self) -> u64 {
+        self.outputs.iter().map(|output| output.value).sum()
+    }
+
+    pub fn input_hashes(&self) -> HashSet<Blockhash> {
+        self.inputs
+            .iter()
+            .map(|input| input.hash())
+            .collect::<HashSet<Blockhash>>()
+    }
+
+    pub fn output_hashes(&self) -> HashSet<Blockhash> {
+        self.outputs
+            .iter()
+            .map(|output| output.hash())
+            .collect::<HashSet<Blockhash>>()
+    }
+
+    pub fn is_coinbase(&self) -> bool {
+        self.inputs.len() == 0
+    }
 }
 
 impl Hashable for Transaction {
