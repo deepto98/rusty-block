f07f57a75185d6ee8a3e134da86d07b21343c3dd
deepto <ddepp98@outlook.com>
2023-06-01 18:51:43 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use blockchainlib::*;
 
 fn main() {
-    let block = Block::new(0, now(), vec![0; 32], 0, "Genesis Block".to_owned());
+    let mut block = Block::new(0, now(), vec![0; 32], 0, "Genesis Block".to_owned());
     println!("{:?}", &block);
 
+    // How the hash  works - Block implements Hashable.bytes. that is used in Hashable.hash() to generate the hash for the block
     let h = block.hash();
 
     println!("{:?}", &h);
+
+    //Set generated hash as hash of current block
+    block.hash=h;
 }
