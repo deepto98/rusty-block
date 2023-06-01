0d9c406b2fde82f909d66229fa7d0dcd3528bc4b
deepto <ddepp98@outlook.com>
2023-06-01 18:44:53 +0530

    Implemented hashable trait's bytes for Cargo. Current state : block gets printed properly

--- src/lib.rs
+++ src/lib.rs
@@ -64,4 +64,4 @@ mod block;
 pub use crate::block::Block;
 
 mod hashable;
-pub use crate::hashable::hashable;
+pub use crate::hashable::Hashable;
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ use blockchainlib::*;
 
 fn main() {
     let block = Block::new(0, now(), vec![0; 32], 0, "Genesis Block".to_owned());
-    println!("{:?}", &block)
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
 }
