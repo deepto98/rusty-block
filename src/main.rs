e264c867555ac906f94fe00dd079ad15e0d0d82c
deepto <ddepp98@outlook.com>
2023-06-02 01:34:10 +0530

    implemented blockchain creation

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,55 @@
 use blockchainlib::*;
 
 fn main() {
-    let difficulty = 0x00ffffffffffffffffffffffffffffff;//Adding more zeroes upfront increases difficulty, hence time to mine and nonce value
+    let difficulty = 0x00ffffffffffffffffffffffffffffff; //Adding more zeroes upfront increases difficulty, hence time to mine and nonce value
 
-    let mut block = Block::new(0, now(), vec![0; 32], 0, "Genesis Block".to_owned(), difficulty);
+    let mut block = Block::new(
+        0,
+        now(),
+        vec![0; 32],
+        0,
+        "Genesis Block".to_owned(),
+        difficulty,
+    );
 
-    // How the hash  works - Block implements Hashable.bytes. that is used in Hashable.hash() to generate the hash for the block
-    let h = block.hash();
+    // // How the hash  works - Block implements Hashable.bytes. that is used in Hashable.hash() to generate the hash for the block
+    // let h = block.hash();
 
-    // println!("{:?}", &h);
+    // //Set generated hash as hash of current block
+    // block.hash = h;
+    // // println!("{:?}", &h);
 
-    //Set generated hash as hash of current block
-    block.hash=h;
-
-    println!("{:?}", &block);
-
+    // // Genesis block
+    // println!("{:?}", &block);
 
     block.mine();
 
-    println!("{:?}", &block);
+    // Mined genesis block
+    println!("Mined genesis block {:?}", &block);
 
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    // Add more blocks
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash,
+            0,
+            "Another Block".to_owned(),
+            difficulty,
+        );
+
+        block.mine();
+
+        println!("Mined genesis block {:?}", &block);
+
+        last_hash =block.hash.clone();
+
+        blockchain.blocks.push(block);
+    }
 }
